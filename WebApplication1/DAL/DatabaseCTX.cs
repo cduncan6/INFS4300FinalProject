35201f4c4d020e6d54f5db06e7a23f546e63a86e
Cody Duncan <cdresilient@gmail.com>
2017-04-11 15:31:40 -0400

    Update2
    Update using Jacob's code.

---
+++ WebApplication1/DAL/DatabaseCTX.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.DAL
+{   
+        public class DatabaseCTX : DbContext
+        {
+            public DbSet<Employee> Employees { get; set; }
+
+        }
+}
+   
